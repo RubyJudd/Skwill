2b1db56b38e185dad6e334d24614c01b5f7c1790
Ruby <ruby.rj@gmail.com>
2021-02-06 08:52:10 +0000

    initial set-up

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+
+app.set("view engine",  "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res){	
+		res.render("landing");
+});
+
+app.get("/illustrations", function(req, res){	
+		res.render("illustrations");
+});
+
+app.get("/photography", function(req, res){	
+		res.render("photography");
+});
+
+app.get("/videography", function(req, res){	
+		res.render("videography");
+});
+
+app.get("/animations", function(req, res){	
+		res.render("animations");
+});
+
+app.get("/graphicdesigns", function(req, res){	
+		res.render("graphicdesigns");
+});
+
+app.get("/websites", function(req, res){	
+		res.render("websites");
+});
+
+app.get("/shortstories", function(req, res){	
+		res.render("shortstories");
+});
+
+app.get("/pastclients", function(req, res){	
+		res.render("pastclients");
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started");
+});
+
+
